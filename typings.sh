9ff13d6d193ebe2651880f9145c37db1f9a19f9c
ammarahm-ed <ammarahmed6506@gmail.com>
2022-08-28 23:36:55 +0500

    add typings generator script

---
+++ typings.sh
@@ -0,0 +1,1 @@
+cd apps/demo/ && ns typings android --aar ../../packages/react-native/platforms/android/react.aar && rm -rf ../../packages/react-native/typings &&  cp -r typings ../../packages/react-native/ && cd ../../
