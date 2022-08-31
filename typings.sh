498b545ca73ee7f041223d3206949f0b5dffbc2b
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-08-31 11:11:56 +0900

    fix: ensure script runs in consistent working directory

--- typings.sh
+++ typings.sh
@@ -1,1 +1,4 @@
+# Change directory to root of monorepo (where this file is)
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 cd apps/demo/ && ns typings android --aar ../../packages/react-native/platforms/android/react.aar && rm -rf ../../packages/react-native/typings &&  cp -r typings ../../packages/react-native/ && cd ../../
