1bd3f83635cad65cfe268bf655ff8f3a6c1ef9a2
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-09-14 12:41:28 +0900

    Stop hook test script entering built package

--- packages/react-native-podspecs/test_hook.js
+++ packages/react-native-podspecs/test_hook.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-const monorepoRoot = path.resolve(__dirname, '../../..');
+const monorepoRoot = path.resolve(__dirname, '../..');
 const nsDemoProjectDir = path.resolve(monorepoRoot, 'apps/demo');
 
 const nsDemoPackageJson = JSON.parse(fs.readFileSync(path.join(nsDemoProjectDir, 'package.json')).toString());
 
-require('./before-buildIOS')({
+require('./hooks/before-buildIOS')({
   platformData: {
     normalizedPlatformName: 'iOS',
     platformNameLowerCase: 'ios',
