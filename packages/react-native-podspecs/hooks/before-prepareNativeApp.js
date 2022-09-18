b785dbd1a48371a6d609de745ed2fc73c7609bc3
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-09-18 17:20:41 +0900

    Clean up typings for HookArgs

--- packages/react-native-podspecs/hooks/before-prepareNativeApp.js
+++ packages/react-native-podspecs/hooks/before-prepareNativeApp.js
@@ -10,74 +10,7 @@ const writeFile = promisify(fs.writeFile);
 const logPrefix = '[react-native-podspecs/hooks/before-prepareNativeApp.js]';
 
 /**
- * For all available hook types, see:
- * @see https://github.com/NativeScript/NativeScript/blob/9ccc54b6035c04e383d9256fb3ad9c0c7b00fa20/packages/core/config/config.interface.ts#L134
- * @see https://github.com/NativeScript/nativescript-hook/issues/11#issuecomment-457705213
- * @see https://docs.npmjs.com/cli/v6/using-npm/scripts#pre--post-scripts
- * @see https://github.com/NativeScript/nativescript-app-templates/blob/master/packages/template-hello-world-ts/hooks/after-createProject/after-createProject.js
- * @see https://www.nsplugins.com/plugin/nativescript
- * @see https://github.com/NativeScript/plugins/tree/31b0f5157d02fdf17e89ac29233585cb7e4ebf6f/packages/localize/hooks
- * @see https://github.com/NativeScript/nativescript-cli/blob/master/extending-cli.md
- * @see https://blog.nativescript.org/migrating-cli-hooks-to-nativescript-6.0/
- * @see https://github.com/NativeScript/nativescript-cli/blob/c61edbeef668b9c17e744d7af3909306c435a16a/lib/project-data.ts
- *
- * @param {object} hookArgs
- * @param {object} hookArgs.platformData
- * https://github.com/NativeScript/nativescript-cli/blob/fa257dd455ca55374cf419d638ac74166dc727db/lib/services/android-project-service.ts#L200
- * https://github.com/NativeScript/nativescript-cli/blob/1435eef272aced36e7f97a355ff5c7ea936f94e7/lib/services/ios-project-service.ts#L131
- * @param {string} hookArgs.platformData.frameworkPackageName
- * @param {"iOS"|"Android"} hookArgs.platformData.normalizedPlatformName
- * @param {"ios"|"android"} hookArgs.platformData.platformNameLowerCase
- * @param {string} hookArgs.projectRoot
- *   e.g. /Users/jamie/Documents/git/nativescript-magic-spells/apps/demo/platforms/ios
- * @param {object} hookArgs.projectData
- * https://github.com/NativeScript/nativescript-cli/blob/master/lib/services/android-project-service.ts
- * https://github.com/NativeScript/nativescript-cli/blob/master/lib/services/ios-project-service.ts
- * @param {string} hookArgs.projectData.projectDir
- *   The absolute path to the root of your project. Typically where you'd see
- *   package.json, webpack.config.js, nativescript.config.js, and the platforms
- *   directory.
- * @param {string} hookArgs.projectData.platformsDir
- *   The absolute path to the platforms directory.
- * @param {unknown} hookArgs.projectData.projectConfig
- * @param {string} hookArgs.projectData.projectFilePath
- *   The filepath to the package.json file for your project.
- * @param {{ ios: string; android: string } | string} hookArgs.projectData.projectIdentifiers
- *   I'm unclear of the exact type of this.
- * @param {string} hookArgs.projectData.projectName
- *  The directory name of your projectDir.
- * @param {unknown} hookArgs.projectData.packageJsonData
- *   The full contents of package.json.
- * @param {unknown} hookArgs.projectData.nsConfig
- *   The full contents of nativescript.config.ts. Defined here:
- *   https://github.com/NativeScript/NativeScript/blob/9ccc54b6035c04e383d9256fb3ad9c0c7b00fa20/packages/core/config/config.interface.ts#L153
- * @param {string} hookArgs.projectData.appDirectoryPath
- * @param {string} hookArgs.projectData.appResourcesDirectoryPath
- *   From nativescript.config.ts. The absolute path to App_Resources.
- * @param {Record<string, string>} hookArgs.projectData.dependencies
- *   From package.json. The record of dependencies.
- * @param {Record<string, string>} hookArgs.projectData.devDependencies
- *   From package.json. The record of devDependencies.
- * @param {string[]} hookArgs.projectData.ignoredDependencies
- *   From nativescript.config.ts. Optionally specify a list of npm package names
- *   for which you would like the NativeScript CLI to ignore when attaching
- *   native dependencies to the build.
- * @param {string} hookArgs.projectData.projectType
- *   The renderer or template, e.g. "Angular", "Vue.js", "React", "Svelte",
- *   "Pure TypeScript", "Pure JavaScript".
- *   https://github.com/NativeScript/nativescript-cli/blob/3150049f8076b12fed7deb86dc221490c8d13394/test/project-data.ts#L93
- * @param {string} hookArgs.projectData.androidManifestPath
- * @param {string} hookArgs.projectData.infoPlistPath
- * @param {string} hookArgs.projectData.appGradlePath
- * @param {string} hookArgs.projectData.gradleFilesDirectoryPath
- * @param {string} hookArgs.projectData.buildXcconfigPath
- * @param {string} hookArgs.projectData.podfilePath
- * @param {boolean} hookArgs.projectData.isShared
- * @param {string} hookArgs.projectData.previewAppSchema
- * @param {string} hookArgs.projectData.webpackConfigPath
- * @param {boolean} hookArgs.projectData.initialized
- * @param {[string, object, object]} hookArgs.$arguments
- *   Just lists out projectRoot, projectData, and buildData again in an array.
+ * @param {import('./hookArgs').HookArgs} hookArgs
  */
 module.exports = async function (hookArgs) {
   const {
