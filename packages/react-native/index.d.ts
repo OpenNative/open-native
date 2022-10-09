88ac35c05725df22758244086e1a0e48438e0f57
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-10-09 14:30:01 +0900

    chore: prefer inline eslint rule overrides

--- packages/react-native/index.d.ts
+++ packages/react-native/index.d.ts
@@ -1,11 +1,12 @@
-/* eslint-disable no-var */
 import { JSModules } from './js-modules.ios';
 import type DeviceEventEmitter from './device-event-emitter.ios';
 import type Platform from './core/Utilities/Platform';
 import type { Linking } from './core/Libraries/Linking/Linking';
 
 declare global {
+  // eslint-disable-next-line no-var
   var reactNativeBridgeIOS: RCTBridge;
+  // eslint-disable-next-line no-var
   var jsModulesIOS: JSModules;
 }
 
