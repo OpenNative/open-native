590c0baae878883a095da562d92065020b6e1a4f
Nathan Walker <walkerrunpdx@gmail.com>
2022-10-22 09:01:06 -0700

    chore: remove Angular and unneeded transient dependencies

--- apps/demo-angular/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { runNativeScriptAngularApp, platformNativeScript } from '@nativescript/angular';
-import { AppModule } from './app.module';
-
-runNativeScriptAngularApp({
-  appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
-});
