3c958bd659ebf2d31434aa0b90a3534e36c5e9d1
Nathan Walker <walkerrunpdx@gmail.com>
2022-10-28 16:16:40 -0700

    chore: cleanup

--- apps/demo/src/plugin-demos/open-native.ts
+++ apps/demo/src/plugin-demos/open-native.ts
@@ -1,5 +1,5 @@
 import { Observable, EventData, Page } from '@nativescript/core';
-import { DemoSharedReactNative } from '@demo/shared';
+import { DemoSharedOpenNative } from '@demo/shared';
 // import {} from 'open-native';
 
 export function navigatingTo(args: EventData) {
@@ -7,4 +7,4 @@ export function navigatingTo(args: EventData) {
   page.bindingContext = new DemoModel();
 }
 
-export class DemoModel extends DemoSharedReactNative {}
+export class DemoModel extends DemoSharedOpenNative {}
--- tools/demo/open-native/index.ts
+++ tools/demo/open-native/index.ts
@@ -1,25 +1,25 @@
-import { isAndroid } from '@nativescript/core';
+// import { isAndroid } from '@nativescript/core';
 //@ts-ignore
-//import Auth0 from 'react-native-auth0';
+import Auth0 from 'react-native-auth0';
 import { DemoSharedBase } from '../utils';
 import { NativeModules } from 'open-native';
-//const auth0 = new Auth0({ domain: 'https://dev-mr5werjm.us.auth0.com', clientId: 'xbUxJUQ5ofi7qoRHn3YjZp6A1f7qdZ5n' });
+const auth0 = new Auth0({ domain: 'https://dev-mr5werjm.us.auth0.com', clientId: 'xbUxJUQ5ofi7qoRHn3YjZp6A1f7qdZ5n' });
 
-export class DemoSharedReactNative extends DemoSharedBase {
+export class DemoSharedOpenNative extends DemoSharedBase {
   loadBridge() {
     //console.log(NativeModules.RNTestModule);
-    console.time('promise-test');
-    NativeModules.RNTestModuleAliased.show()
-      .then(() => {
-        console.timeEnd('promise-test');
-      })
-      .catch((e) => console.log(e));
+    // console.time('promise-test');
+    // NativeModules.RNTestModuleAliased.show()
+    //   .then(() => {
+    //     console.timeEnd('promise-test');
+    //   })
+    //   .catch((e) => console.log(e));
 
-    // auth0.webAuth
-    //   .authorize({ scope: 'openid profile email' })
-    //   .then((credentials) => console.log(credentials))
-    //   .catch((error) => console.log(error));
-    // NativeModules.A0Auth0.showUrl('dev-mr5werjm.us.auth0.com', true, false, (e, r) => {
+    auth0.webAuth
+      .authorize({ scope: 'openid profile email' })
+      .then((credentials) => console.log(credentials))
+      .catch((error) => console.log(error));
+    // NativeModules.A0Auth0.showUrl('https://dev-mr5werjm.us.auth0.com', true, false, (e, r) => {
     //   console.log(e, r);
     // });
     // const RCTLink = new RCTLinkingManager();
@@ -31,7 +31,7 @@ export class DemoSharedReactNative extends DemoSharedBase {
   }
 
   getName() {
-    // console.log(Object.keys(NativeModules));
+    console.log(Object.keys(NativeModules));
   }
   callbackTest() {
     console.log('to do');
