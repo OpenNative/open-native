008c79fe327e73da719784c3f27ca01e6a600c92
ammarahm-ed <ammarahmed6506@gmail.com>
2022-11-13 19:09:33 +0500

    fix typings generation

--- typings.sh
+++ typings.sh
@@ -1,4 +1,4 @@
 # Change directory to root of monorepo (where this file is)
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-cd apps/demo/ && ns typings android --aar ../../dist/packages/core/platforms/android/react.aar && rm -rf ../../packages/core/typings &&  cp -r typings ../../packages/core/ && cd ../../
+cd apps/demo/ && ns typings android --aar ../../packages/core/platforms/android/react.aar && rm -rf ../../packages/core/typings/android &&  cp -r typings/android ../../packages/core/typings && cd ../../
