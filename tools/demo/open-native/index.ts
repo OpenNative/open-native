97ecb5d5e4d5bc51ee63aa59150ecdb82501a2c6
Nathan Walker <walkerrunpdx@gmail.com>
2022-11-14 13:19:40 -0800

    chore: testing release builds

--- tools/demo/open-native/index.ts
+++ tools/demo/open-native/index.ts
@@ -1,5 +1,9 @@
 import { DemoSharedBase } from '../utils';
 import { NativeModules } from '@open-native/core';
+//@ts-ignore
+import Auth0 from 'react-native-auth0';
+
+const auth0 = new Auth0({ domain: 'https://dev-mr5werjm.us.auth0.com', clientId: 'xbUxJUQ5ofi7qoRHn3YjZp6A1f7qdZ5n' });
 
 async function measure(name: string, action: () => Promise<void>) {
   const start = performance.now();
@@ -18,5 +22,12 @@ export class DemoSharedOpenNative extends DemoSharedBase {
       }
     };
     measure('promise', testPromise);
+  }
+
+  tryAuth0() {
+    auth0.webAuth
+      .authorize({ scope: 'openid profile email' })
+      .then((credentials) => console.log(credentials))
+      .catch((error) => console.log(error));
   }
 }
