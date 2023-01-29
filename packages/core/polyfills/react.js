e0a358fd59a36f502e7a6e182b527298d94ee5bc
ammarahm-ed <ammarahmed6506@gmail.com>
2023-01-29 10:05:05 +0500

    auto polyfill react package if not installed

---
+++ packages/core/polyfills/react.js
@@ -0,0 +1,19 @@
+export const useState = () => null;
+export const createContext = () => null;
+export const useEffect = () => null;
+export const useContext = () => null;
+export const useMemo = () => null;
+export const useRef = () => null;
+export const useReducer = () => null;
+export const useCallback = () => null;
+
+export default {
+  useState,
+  useCallback,
+  useContext,
+  createContext,
+  useMemo,
+  useReducer,
+  useRef,
+  useEffect,
+};
