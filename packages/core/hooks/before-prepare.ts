a200a3870b8c514e120fd86a7257f087d91db978
Igor Randjelovic <rigor789@gmail.com>
2023-05-22 14:31:31 +0200

    fix(logs): reset log styles

--- packages/core/hooks/before-prepare.ts
+++ packages/core/hooks/before-prepare.ts
@@ -3,9 +3,9 @@ import type { HookArgs } from './hookArgs';
 import { autolinkAndroid } from './android/prepare';
 import { autolinkIos } from './ios/prepare';
 
-const logPrefix = '[@open-native/core/hooks/before-prepare.js]';
 const green = '\x1b[32m';
 const reset = '\x1b[0m';
+const logPrefix = reset + '[@open-native/core/hooks/before-prepare.js]' + reset;
 
 /**
  * autolink any React Native native modules.
