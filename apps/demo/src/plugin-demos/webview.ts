894d25819e6cdbea44565f8a8798116daf274757
ammarahm-ed <ammarahmed6506@gmail.com>
2023-07-13 22:22:38 +0500

    update packages

--- apps/demo/src/plugin-demos/webview.ts
+++
@@ -1,4 +0,0 @@
-import { Platform } from 'react-native';
-import { requireNativeViewIOS, requireNativeViewAndroid } from '@open-native/core';
-
-export const WebView = Platform.OS === 'ios' ? requireNativeViewIOS('RNCWebView') : requireNativeViewAndroid('RNCWebView' as never);
