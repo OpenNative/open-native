46c02917fdf18b88f35922b346f939754c8d5e11
ammarahm-ed <ammarahmed6506@gmail.com>
2023-07-22 18:10:18 +0500

    make okhttp module optional

--- packages/core/react-android/bridge/src/main/java/com/bridge/Bridge.java
+++ packages/core/react-android/bridge/src/main/java/com/bridge/Bridge.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class Bridge {
@@ -171,6 +172,9 @@ public class Bridge {
     }
   }
 
+  public Set<String> moduleNames() {
+    return Packages.modulePackageMap.keySet();
+  }
 
   public boolean isModuleAvailable(String name) {
     return Packages.moduleClasses.get(name) != null || Packages.modulePackageMap.get(name) != null;
