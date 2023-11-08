2cae235e7d2ed59fc0eed59f2dc3af31b3fae8a1
Ammar Ahmed <ammarahmed6506@gmail.com>
2023-11-08 22:02:52 +0500

    fix dimensions module

--- packages/core/Libraries/Utilities/Dimensions.ts
+++ packages/core/Libraries/Utilities/Dimensions.ts
@@ -1,18 +1,17 @@
 import { Screen } from '@nativescript/core';
 import { getCurrentFontScale } from '@nativescript/core/accessibility';
-import { getRootView } from '@nativescript/core/application';
+import { Application } from '@nativescript/core/application';
 
 class Dimensions {
   static get(type: 'window' | 'screen') {
-    getRootView().width;
     return {
       width:
         type === 'window'
-          ? getRootView().getMeasuredWidth?.() || 0
+          ? Application.getRootView()?.getMeasuredWidth?.() || 0
           : Screen.mainScreen.widthDIPs,
       height:
         type === 'window'
-          ? getRootView().getMeasuredHeight?.() || 0
+          ? Application.getRootView()?.getMeasuredHeight?.() || 0
           : Screen.mainScreen.heightDIPs,
       fontScale: getCurrentFontScale(),
       scale: Screen.mainScreen.scale,
