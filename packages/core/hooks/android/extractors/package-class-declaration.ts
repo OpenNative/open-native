32655bf95bcb9bcc1e072f00235190611de24191
Ammar Ahmed <ammarahmed6506@gmail.com>
2024-07-21 03:22:12 +0500

    android: skip extracting abstract classes

--- packages/core/hooks/android/extractors/package-class-declaration.ts
+++ packages/core/hooks/android/extractors/package-class-declaration.ts
@@ -8,16 +8,22 @@
  * ]
  */
 export function extractClassDeclarationForPackage(file: string) {
-  const nativeModuleMatch = file.match(
-    /class\s+(\w+[^(\s]*)[\s\w():]*(\s+implements\s+|:)[\s\w():,]*[^{]*ReactPackage/
-  );
   // We first check for implementation of ReactPackage to find native modules
   // and then for subclasses of TurboReactPackage to find turbo modules.
-  if (nativeModuleMatch) {
-    return nativeModuleMatch;
-  } else {
-    return file.match(
+
+  const classDeclMatches =
+    file.match(
+      /class\s+(\w+[^(\s]*)[\s\w():]*(\s+implements\s+|:)[\s\w():,]*[^{]*ReactPackage/
+    ) ||
+    file.match(
       /class\s+(\w+[^(\s]*)[\s\w():]*(\s+extends\s+|:)[\s\w():,]*[^{]*TurboReactPackage/
     );
-  }
+
+  if (
+    classDeclMatches &&
+    new RegExp(`abstract\\s+class\\s+${classDeclMatches[1]}`).test(file)
+  )
+    return;
+
+  return classDeclMatches;
 }
